dfc3cb2c5261cd5cfde9f50cbe03cc4a9a3d0a4d
sajidCode <muhammadsajidkhan616@gmail.com>
2023-06-21 03:33:36 +0500

    animating hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,39 @@ import "./assets/font/stylesheet.css";
 import "./App.css";
 import Header from "./layout/header/Header.layout";
 import Main from "./layout/main/main.layout";
+import { useEffect, useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 function App() {
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (event) => {
+        const { clientX: x, clientY: y } = event;
+        setMousePosition({ x, y });
+    };
+
+    useEffect(() => {
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    });
+
+    // const moveMouse = useSpring({
+    //     top: mousePosition.y - 50,
+    //     left: mousePosition.x - 50,
+    // });
+
     return (
         <div className="app">
             <Header />
             <Main />
+
+            <animated.div
+                style={{ top: mousePosition.y, left: mousePosition.x }}
+                className="follow-pointer"
+            ></animated.div>
         </div>
     );
 }
--- src/components/hero/Hero.component.jsx
+++ src/components/hero/Hero.component.jsx
@@ -1,10 +1,51 @@
 import "./Hero.styles.css";
 import mainImage from "./../../assets/hero.png";
 import { useState, useEffect } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 const Hero = () => {
     const [screenHight, setScreenHeight] = useState(window.innerHeight);
+
     const [moveBigText, setMovement] = useState();
+
+    const animateBorder = useSpring({
+        from: { boxShadow: "0 0 1px 2px rgba(15, 20, 30, 0.3)" },
+        to: async (next) => {
+            while (true) {
+                await next({
+                    boxShadow: "0 0 1px 2px rgba(15, 20, 30, 0.3)",
+                });
+                await next({
+                    boxShadow: "0 0 10px 5px rgba(15, 20, 30, 0.6)",
+                });
+            }
+        },
+        config: { duration: 1000 },
+        loop: true,
+    });
+
+    const textAnimateOne = useSpring({
+        from: {
+            opacity: 0,
+            transform: "translateY(100px)",
+        },
+        to: {
+            opacity: 1,
+            transform: "translateY(0px)",
+        },
+    });
+
+    const textAnimateTwo = useSpring({
+        from: {
+            opacity: 0,
+            transform: "translateY(100px)",
+        },
+        to: {
+            opacity: 1,
+            transform: "translateY(0px)",
+        },
+        delay: 500,
+    });
 
     const handleScroll = () => {
         setScreenHeight(window.innerHeight);
@@ -25,8 +66,6 @@ const Hero = () => {
         };
     }, []);
 
-    console.log(moveBigText);
-
     return (
         <div className="hero" style={{ height: screenHight - 104 + "px" }}>
             <div className="big-text" style={{ left: moveBigText + "rem" }}>
@@ -36,21 +75,28 @@ const Hero = () => {
             <div className="hero-section">
                 <div className="hero-text">
                     <h4>MUHAMMAD SAJID</h4>
-                    <h1>
+                    <animated.h1 style={textAnimateOne}>
                         <span>Proficient</span> <br /> Front-end Developer
-                    </h1>
-                    <p className="hero-description">
+                    </animated.h1>
+                    <animated.p
+                        className="hero-description"
+                        style={textAnimateTwo}
+                    >
                         Dedicated to delivering high-quality, visually stunning
                         solutions. Passionate about learning and embracing new
                         technologies. Excited to collaborate on real-world
                         projects, combining performance and aesthetics. Ready to
                         take on new challenges and contribute to your team's
                         success.
-                    </p>
+                    </animated.p>
+
+                    <animated.a href="#contacts" style={animateBorder}>
+                        Hit Me Up
+                    </animated.a>
                 </div>
-                <div className="hero-image">
+                <animated.div style={textAnimateOne} className="hero-image">
                     <img src={mainImage} alt="main image" />
-                </div>
+                </animated.div>
             </div>
         </div>
     );
