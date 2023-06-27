55a12307f7c934e393230e81f7aeca0f14804249
sajidCode <muhammadsajidkhan616@gmail.com>
2023-06-28 00:15:20 +0500

    corrected base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://github.com/sajidCode-gh/sajid-portfolio",
+    base: "https://github.com/sajidCode-gh/sajid-portfolio/tree/gh-pages",
 });
