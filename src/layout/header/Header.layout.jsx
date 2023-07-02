447b27923ad9c41a22df78f820844d12e06856b5
sajidCode <muhammadsajidkhan616@gmail.com>
2023-07-02 20:56:21 +0500

    hide and show header based on scroll direction

--- src/layout/header/Header.layout.jsx
+++ src/layout/header/Header.layout.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "../../components/logo/Logo.component";
 import Navbar from "../../components/navbar/Navbar.component";
 import "./Header.styles.css";
+import { useSpring, animated } from "@react-spring/web";
 
 const Header = () => {
     const [toggle, setToggle] = useState(true);
+    const [previousScroll, setPreviousScroll] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScroll = window.pageYOffset;
+            setVisible(currentScroll < previousScroll);
+            setPreviousScroll(currentScroll);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [previousScroll]);
+
+    const headerShowHide = useSpring({
+        transform: visible ? "translateY(0%)" : "translateY(-100%)",
+    });
 
     return (
-        <header className="container header">
+        <animated.header style={headerShowHide} className="container header">
             <Logo toggle={toggle} />
             <Navbar toggle={toggle} handleToggle={setToggle} />
-        </header>
+        </animated.header>
     );
 };
 
